337b9a110efb28b760ad8175937c334b16fcaf3f
Johan Fransson <johan.fransson@polystar.com>
2019-11-30 14:32:11 +0100

    Fixed broken Eclipse C dialect

---
+++ src/diskrenderer.c
@@ -0,0 +1,4 @@
+#include "diskrenderer.h"
+
+void diskrenderer_renderSong(Song *song) {
+}
---
+++ src/diskrenderer.h
@@ -0,0 +1,7 @@
+#ifndef DISKRENDERER_H_
+#define DISKRENDERER_H_
+
+void diskrenderer_renderSong(Song *song);
+
+
+#endif /* DISKRENDERER_H_ */
